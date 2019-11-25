7bbbf128cd6c803f1c0de7ccefc9e24b3e9a0e34
gianmillare <gianmillaregsm@gmail.com>
2019-11-24 21:36:34 -0800

    adding more code to app.js

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -3,16 +3,22 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
+  d3.json(`/metadata/${sample}`).then((data) => {
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    var panel = d3.select("#sample-metadata");
     // Use `.html("") to clear any existing metadata
-
+    panel.html("");
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
-
+    Object.entries(data).forEach(([key, value]) => {
+      panel.append("h6").text(`${key}: ${value}`);
+      console.log(key, value);
+    });
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+    
+  });  
 }
 
 function buildCharts(sample) {
